5e489148c4216c3d14b90bbb6aadc5f18f5440f8
Jakub Głusiński <kuba.g777@gmail.com>
2023-12-24 11:55:17 +0100

    login.php sign up button pc

--- public/views/login.php
+++ public/views/login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 
 <head>
+    <link rel="stylesheet" type="text/css" href="public/css/global.css">
     <link rel="stylesheet" type="text/css" href="public/css/login.css">
     <title>LOGIN PAGE</title>
 </head>
@@ -8,23 +9,23 @@
 <body>
     <div class="container">
         <div class="overlap-group">
-        <div class="page-header"><div class="login-text">Log In</div></div>
-        <div class="button-signUp"><div class="SignUp">Sign Up</div></div>
-        <form class="login" action="login" method="POST">
-            <div class="messages">
-                <?php
-                if(isset($messages)){
-                    foreach($messages as $message) {
-                        echo $message;
+            <div class="page-header"><div class="login-text">Log In</div></div>
+            <div class="button-signUp"><div class="SignUp">Sign Up</div></div>
+            <form class="login" action="login" method="POST">
+                <div class="messages">
+                    <?php
+                    if(isset($messages)){
+                        foreach($messages as $message) {
+                            echo $message;
+                        }
                     }
-                }
-                ?>
-            </div>
-            <input name="input-text" type="text" placeholder="email@email.com">
-            <input name="pass" type="password" placeholder="password">
-            <button type="loginButton">LOGIN</button>
-        </form>
-        <div class="forgot">Forgot your password?</div>
+                    ?>
+                </div>
+                <input name="input-text" type="text" placeholder="email@email.com">
+                <input name="pass" type="password" placeholder="password">
+                <button type="loginButton">LOGIN</button>
+            </form>
+        </div>
+        <div class="forgot"><a href="">Forgot your password?</a></div>
     </div>
-
 </body>
