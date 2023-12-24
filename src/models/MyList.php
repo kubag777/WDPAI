2e74817cacef8c263c585a2bc6dae6c959a8c825
Jakub Głusiński <kuba.g777@gmail.com>
2023-12-24 16:09:02 +0100

    Lists and fields models

---
+++ src/models/MyList.php
@@ -0,0 +1,57 @@
+<?php
+
+class MyList {
+    private $name;
+    private $users = [];
+    private $fields = [];
+
+    public function __construct(
+        string $name,
+        array $users = []
+    ) {
+        $this->name = $name;
+        foreach ($users as $user) {
+            if ($user instanceof User) {
+                $this->addUser($user);
+            }
+        }
+    }
+
+    public function addUser(User $user) {
+        $this->users[] = $user;
+    }
+
+    public function getUsers() {
+        return $this->users;
+    }
+    public function removeUser(User $userToRemove) {
+        $indexToRemove = null;
+        foreach ($this->users as $index => $user) {
+            if ($user === $userToRemove) {
+                $indexToRemove = $index;
+                break;
+            }
+        }
+        if ($indexToRemove !== null) {
+            unset($this->users[$indexToRemove]);
+        }
+    }
+    public function addField(MyFields $field) {
+        $this->fields[] = $field;
+    }
+    public function getFields() {
+        return $this->fields;
+    }
+    public function removeField(MyFields $fieldToRemove) {
+        $indexToRemove = null;
+        foreach ($this->fields as $index => $field) {
+            if ($field === $fieldToRemove) {
+                $indexToRemove = $index;
+                break;
+            }
+        }
+        if ($indexToRemove !== null) {
+            unset($this->fields[$indexToRemove]);
+        }
+    }
+}
